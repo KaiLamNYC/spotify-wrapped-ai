749703ea52e3935039bb1885eaa817d28a9e1a7c
Kai Lam <kailamnyc@gmail.com>
2023-09-17 15:56:36 -0400

    dialog stuff

--- components/generate/Songs.tsx
+++ components/generate/Songs.tsx
@@ -48,13 +48,18 @@ const Songs = (props: Props) => {
 					</DialogHeader>
 					<Input placeholder='Enter song' type='text' />
 					<Button type='submit'>Search</Button>
-					<ScrollArea className='h-72 w-48 rounded-md border'>
+					<ScrollArea className='h-72 w-full rounded-md border'>
 						<div className='p-4'>
-							<h4>SONGS</h4>
+							<h4 className='mb-4 text-sm font-medium leading-none'>SONGS</h4>
 							{tags.map((tag) => (
 								<>
-									<div key={tag} className='text-sm'>
-										{tag}
+									<div key={tag} className='text-sm flex justify-between'>
+										<div>
+											<p>{tag}</p>
+											<p>artist</p>
+										</div>
+
+										<Button>Add</Button>
 									</div>
 									<Separator className='my-2' />
 								</>
