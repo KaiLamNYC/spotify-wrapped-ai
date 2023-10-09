1205c22b8c88e95bcfdd7b7fcfb53526034ea94a
Kai Lam <kailamnyc@gmail.com>
2023-10-09 00:53:11 -0400

    fixed hydration issue

--- app/playlist/[id]/page.tsx
+++ app/playlist/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/components/Navbar";
-import { Table, TableCell, TableRow } from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { getPlaylist } from "@/lib/user.actions";
 import Image from "next/image";
 import React from "react";
@@ -25,34 +25,30 @@ const PlaylistPage = async ({ params }: { params: { id: string } }) => {
 						</div>
 					))}
 				</div> */}
-				{/* <Table className='gap-1'>
-					{playlist[0].songs.map((track, index) => (
-						<TableRow key={index}>
-							<TableCell className='font-bold'>{index + 1}</TableCell>
-							<TableCell>
-								<Image
-									src={track.img}
-									alt='album photo'
-									width={40}
-									height={40}
-									// className='p-1'
-								/>
-							</TableCell>
-							<TableCell className='font-semibold'>{track.name}</TableCell>
-							<TableCell>{track.artist}</TableCell>
-							<TableCell>{track.album}</TableCell>
-						</TableRow>
-					))}
-				</Table> */}
+
 				{playlist.length === 0 ? (
 					<p>LOADING</p>
 				) : (
-					<Table>
-						{playlist[0].songs.map((track, index) => (
-							<TableRow key={index}>
-								<TableCell>{track.name}</TableCell>
-							</TableRow>
-						))}
+					<Table className='gap-1'>
+						<TableBody>
+							{playlist[0].songs.map((track, index) => (
+								<TableRow key={index}>
+									<TableCell className='font-bold'>{index + 1}</TableCell>
+									<TableCell>
+										<Image
+											src={track.img}
+											alt='album photo'
+											width={40}
+											height={40}
+											// className='p-1'
+										/>
+									</TableCell>
+									<TableCell className='font-semibold'>{track.name}</TableCell>
+									<TableCell>{track.artist}</TableCell>
+									<TableCell>{track.album}</TableCell>
+								</TableRow>
+							))}
+						</TableBody>
 					</Table>
 				)}
 			</div>
